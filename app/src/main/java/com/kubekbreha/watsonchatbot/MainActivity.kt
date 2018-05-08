2f2ae5affbcf018b2e711ecf3f7784efd144a524
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-08 14:01:34 +0200

    no titlebar

--- app/src/main/java/com/kubekbreha/watsonchatbot/MainActivity.kt
+++ app/src/main/java/com/kubekbreha/watsonchatbot/MainActivity.kt
@@ -14,41 +14,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         configureTabLayout()
 
     }
 
     private fun configureTabLayout() {
 
-        val tab_layout = findViewById<TabLayout>(R.id.tab_layout)
-
-        tab_layout.addTab(tab_layout.newTab().setText("Tab 1 Item"))
-        tab_layout.addTab(tab_layout.newTab().setText("Tab 2 Item"))
+        tab_layout.addTab(tab_layout.newTab().setIcon(R.drawable.ic_peoples_white))
+        tab_layout.addTab(tab_layout.newTab().setIcon(R.drawable.ic_profile_white))
 
 
-        val adapter = TabPagerAdapter(supportFragmentManager,
-                tab_layout.tabCount)
+        val adapter = TabPagerAdapter(supportFragmentManager, tab_layout.tabCount)
         pager.adapter = adapter
 
-        pager.addOnPageChangeListener(
-                TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+        pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+
         tab_layout.addOnTabSelectedListener(object :
                 TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 pager.currentItem = tab.position
             }
 
-            override fun onTabUnselected(tab: TabLayout.Tab) {
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
 
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {
-
-            }
+            override fun onTabReselected(tab: TabLayout.Tab) {}
 
         })
+
     }
 
 
