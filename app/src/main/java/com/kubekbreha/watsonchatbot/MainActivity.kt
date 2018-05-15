b86f9317548b65bd68b1d30c2de7868e4b35b207
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-15 21:26:16 +0200

    vipe out tab layout and fragments

--- app/src/main/java/com/kubekbreha/watsonchatbot/MainActivity.kt
+++ app/src/main/java/com/kubekbreha/watsonchatbot/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 
 import android.support.v7.app.AppCompatActivity
-import com.ebookfrenzy.tablayoutdemo.TabPagerAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,33 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        configureTabLayout()
+        
 
     }
 
-    private fun configureTabLayout() {
 
-        tab_layout.addTab(tab_layout.newTab().setIcon(R.drawable.ic_peoples_white))
-        tab_layout.addTab(tab_layout.newTab().setIcon(R.drawable.ic_profile_white))
-
-
-        val adapter = TabPagerAdapter(supportFragmentManager, tab_layout.tabCount)
-        pager.adapter = adapter
-
-        pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
-
-        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                pager.currentItem = tab.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {}
-
-            override fun onTabReselected(tab: TabLayout.Tab) {}
-
-        })
-
-    }
 
 
 }
