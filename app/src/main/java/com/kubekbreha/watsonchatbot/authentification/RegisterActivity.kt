8f26a02474fbbbf3e1470626e078887c41c6b32c
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-15 19:19:49 +0200

    google login added

--- app/src/main/java/com/kubekbreha/watsonchatbot/authentification/RegisterActivity.kt
+++ app/src/main/java/com/kubekbreha/watsonchatbot/authentification/RegisterActivity.kt
@@ -11,7 +11,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.kubekbreha.watsonchatbot.MainActivity
@@ -33,11 +39,23 @@ class RegisterActivity : AppCompatActivity() {
     private var btnCreateAccount: Button? = null
     private var mProgressBar: ProgressDialog? = null
     private var btnBack: ImageButton? = null
+    private var btnGoogle: Button? = null
 
     //Firebase references
     private var mDatabaseReference: DatabaseReference? = null
     private var mDatabase: FirebaseDatabase? = null
     private var mAuth: FirebaseAuth? = null
+
+
+    //Request codes
+    val GOOGLE_LOG_IN_RC = 1
+    val FACEBOOK_LOG_IN_RC = 2
+    val TWITTER_LOG_IN_RC = 3
+
+    // Google API Client object.
+    var googleApiClient: GoogleApiClient? = null
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,20 +66,36 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun initialise() {
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.request_client_id))
+                .requestEmail()
+                .build()
+
+        // Creating and Configuring Google Api Client.
+        googleApiClient = GoogleApiClient.Builder(this@RegisterActivity)
+                .enableAutoManage(this@RegisterActivity  /* OnConnectionFailedListener */) { }
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build()
 
         etUsername = findViewById<View>(R.id.et_username) as EditText
         etEmail = findViewById<View>(R.id.et_email) as EditText
         etPassword = findViewById<View>(R.id.et_password) as EditText
         btnCreateAccount = findViewById<View>(R.id.btn_register) as Button
         btnBack = findViewById<View>(R.id.btn_back_from_register) as ImageButton
+        btnGoogle = findViewById<View>(R.id.register_button_google) as Button
+
         mProgressBar = ProgressDialog(this)
         mDatabase = FirebaseDatabase.getInstance()
         mDatabaseReference = mDatabase!!.reference!!.child("Users")
-        mAuth = FirebaseAuth.getInstance()
-        btnCreateAccount!!.setOnClickListener { createNewAccount() }
-        btnBack!!.setOnClickListener {  finish () }
-    }
 
+        btnCreateAccount!!.setOnClickListener { createNewAccount() }
+        btnBack!!.setOnClickListener { finish() }
+
+        //google login
+        btnGoogle!!.setOnClickListener { googleButtonOnClick() }
+
+        mAuth = FirebaseAuth.getInstance()
+    }
 
 
     private fun createNewAccount() {
@@ -70,12 +104,13 @@ class RegisterActivity : AppCompatActivity() {
         email = etEmail?.text.toString()
         password = etPassword?.text.toString()
 
+
+
         if (!TextUtils.isEmpty(userName)
                 && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
 
             mProgressBar!!.setMessage("Registering User...")
             mProgressBar!!.show()
-
 
             mAuth!!
                     .createUserWithEmailAndPassword(email!!, password!!)
@@ -103,8 +138,27 @@ class RegisterActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show()
         }
+
+
+
+
     }
 
+
+    fun googleButtonOnClick() {
+
+        Toast.makeText(this@RegisterActivity, "Trying google login.",
+                Toast.LENGTH_SHORT).show()
+        googleLogin()
+
+    }
+
+
+    private fun googleLogin() {
+        Log.i(TAG, "Starting Google LogIn Flow.")
+        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient)
+        startActivityForResult(signInIntent, GOOGLE_LOG_IN_RC)
+    }
 
     private fun updateUserInfoAndUI() {
         //start next activity
@@ -129,5 +183,36 @@ class RegisterActivity : AppCompatActivity() {
                                 Toast.LENGTH_SHORT).show()
                     }
                 }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i(TAG, "Got Result code ${requestCode}.")
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == GOOGLE_LOG_IN_RC) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            Log.i(TAG, "With Google LogIn, is result a success? ${result.isSuccess}.")
+            if (result.isSuccess) {
+                // Google Sign In was successful, authenticate with Firebase
+                firebaseAuthWithGoogle(result.signInAccount!!)
+            } else {
+                Toast.makeText(this@RegisterActivity, "Some error occurred.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.i(TAG, "Authenticating user with firebase.")
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth?.signInWithCredential(credential)?.addOnCompleteListener(this) { task ->
+            Log.i(TAG, "Firebase Authentication, is result a success? ${task.isSuccessful}.")
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.e(TAG, "Authenticating with Google credentials in firebase FAILED !!")
+            }
+        }
     }
 }
