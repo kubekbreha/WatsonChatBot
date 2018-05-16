9e33f82b28d6f964931140332370b9f9c432647a
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-16 22:39:27 +0200

    transparent action bar programatically

--- app/src/main/java/com/kubekbreha/watsonchatbot/authentification/LoginActivity.kt
+++ app/src/main/java/com/kubekbreha/watsonchatbot/authentification/LoginActivity.kt
@@ -2,11 +2,13 @@ package com.kubekbreha.watsonchatbot.authentification
 
 import android.app.ProgressDialog
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -38,6 +40,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
 
         initialise()
     }
