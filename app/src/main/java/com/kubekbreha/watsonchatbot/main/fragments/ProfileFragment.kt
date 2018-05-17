f848258a893a06dcf8a51ddd0d6f91abadffc892
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-17 23:30:31 +0200

    fragments styled

--- app/src/main/java/com/kubekbreha/watsonchatbot/main/fragments/ProfileFragment.kt
+++ app/src/main/java/com/kubekbreha/watsonchatbot/main/fragments/ProfileFragment.kt
@@ -16,7 +16,7 @@ class ProfileFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
         return view
     }
 
