0f52f9dfdc1f72fce5e6b9b1df1f0b7387a7ec5e
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-22 17:23:33 +0200

    chat activity styled

--- app/src/main/java/com/kubekbreha/watsonchatbot/main/ChatActivity.kt
+++ app/src/main/java/com/kubekbreha/watsonchatbot/main/ChatActivity.kt
@@ -2,12 +2,14 @@ package com.kubekbreha.watsonchatbot.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Toast
 import com.google.firebase.firestore.ListenerRegistration
 import com.kubekbreha.watsonchatbot.AppConstants
 import com.kubekbreha.watsonchatbot.R
 import com.kubekbreha.watsonchatbot.util.FirestoreUtil
 import com.xwray.groupie.kotlinandroidextensions.Item
+import kotlinx.android.synthetic.main.activity_chat.*
 
 
 class ChatActivity : AppCompatActivity() {
@@ -18,8 +20,12 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
+        //show transparent activity tab
+        val w = window // in Activity's onCreate() for instance
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = intent.getStringExtra(AppConstants.USER_NAME)
+        act_chat_chatting_with?.text = intent.getStringExtra(AppConstants.USER_NAME)
 
 
         val otherUserId = intent.getStringExtra(AppConstants.USER_ID)
